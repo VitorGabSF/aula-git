84767b1104d4e3af9764ef6dd65979b023c7d0d1
geovannadarosa <geovannadarosava@gmail.com>
2026-07-07 15:28:19 -0400

    commit for branch

---
+++ app/Core/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Core;
+
+use PDO;
+
+abstract class Model{
+    public static function pegarBanco(): PDO {
+        return Database::conctar();
+    }
+}
---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'app_name' => 'controlShop',
+    'base_folder' => 'aula/git',
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'controlshop',
+        'user' => 'root',
+        'password' => ''
+    ]
+];
