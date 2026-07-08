4dd55f1d32f80aa4241029d179ecc9ee4d137f0e
Arthur-programer <arthur.recalde.braga@gmail.com>
2026-07-08 16:22:07 -0400

    merge

---
+++ app/Core/model.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Core;
+
+use PDO;
+use Database;
+
+class Model{
+    public static function pegarBanco(): PDO {
+        return Database::conectar();
+    }
+}
---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return[
+    'app_name' => 'ControlShop',
+    'base_folder' => '/aula-git/'
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'controlshop',
+        'user' => 'root',
+        'password' => ''
+    ]
+    ];
