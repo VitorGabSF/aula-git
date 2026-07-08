bddd29fb1d43dd2cf15c351d90ea38c21003dd6f
Gustavo Martins Villa <gustavomt058@gmail.com>
2026-07-08 16:21:22 -0400

    FEAT: controller e router

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+spl_autoload_register(function ($class){
+    $file = __DIR__.'/app/' . str_replace('\\', '/', $class) . '.php';
+
+    if ( file_exists($file)){
+        require $fire;
+    }
+});
+
+$config = require __DIR__.'/config/config.php';
+
+define('BASE_URL', $config['base_folder']);
+
