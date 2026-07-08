a1baee4a416d2e76c020b21b85e461f4911a255b
geovannadarosa <geovannadarosava@gmail.com>
2026-07-08 17:24:03 -0400

    mais uma aula pra cota

---
+++ app/Core/Validador.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core;
+
+class Validador {
+    public static function required(string $valor) : bool {
+        return trim((string) $valor) !== '';
+    }
+
+    public static function email(?string $valor) : bool {
+        return filter_var($valor, FILTER_VALIDATE_EMAIL) !== false;
+    }
+}
