09e2fba3b1541040d509a286a2e46fcba3e7d8f1
Gustavo Martins Villa <gustavomt058@gmail.com>
2026-07-09 16:05:47 -0400

    merge: develop

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -30,6 +30,32 @@ class Router{
             if ($rota['metodo'] !== $metodo || $rota ['uri'] !== $uri){
                 continue;
             }
+
+            if ( $rota['permissao'] !== null){
+                exit;
+            }
+
+            [$controllerNome, $metodoNome] = explode( '@', $rota['controller'], 2);
+
+            $controllerClass = 'Controllers\\' . $controllerNome;
+
+            if (!class_exists($controllerClass)){
+                http_response_code(500);
+                exit('Não tem essa controller');
+            }
+
+            $controller = new $controllerClass();
+
+            if(!method_exists($controller, $metodoNome)){
+                http_response_code(500);
+                exit;
+            }       
+            
+            $controller->$metodoNome();
+            return;
         } 
+
+        http_response_code(404);
+        echo 'Página não encontrada';
     }
 }
