80bf074f24930a0210e098db08ad8791535b485d
Vitor Farias <vitor.farias@edu.ms.senac.br>
2026-07-10 15:09:57 -0400

    FEAT: finalizando a Core Auth e iniciando a AuthController

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Controller;
+
+use Core\Auth;
+use Core\Controller;
+
+class AuthController extends Controller {
+
+    public function loginForm() : void {
+        if (Auth::checar()) {
+            $this->redirecionar('/dashboard');
+        }
+
+        $this->view('auth/login', [
+            'erro'      => Auth::pegarFlash('erro'),
+            'sucesso'   => Auth::pegarFlash('sucesso')
+        ]);
+    }
+}
